2de981ce2d2fe0cea532868dcb53b569d0d69141
Aslan Khetagurov <aslankhetagurovv@gmail.com>
2026-05-13 13:03:19 +0300

    Remove loading text from Spinner

--- src/UI/Spinner.tsx
+++ src/UI/Spinner.tsx
@@ -1,7 +1,7 @@
 const Spinner = () => {
     return (
-        <div className="flex flex-col items-center justify-center h-screen bg-no-repeat bg-cover bg-right-top">
-            <div className="relative mb-8 flex items-center justify-center">
+        <div className="flex items-center justify-center h-screen">
+            <div className="relative flex items-center justify-center">
                 <img
                     className="max-w-28 mt-3.5"
                     src="/logo.png"
@@ -13,11 +13,9 @@ const Spinner = () => {
                     height={50}
                 />
 
-                <div className="h-28 w-28 border-2 rounded-full border-y-[#2eadc4] absolute animate-spin"></div>
+                <div className="h-28 w-28 border-2 rounded-full border-x-[#497ec3] absolute animate-spin" />
             </div>
-            <p className="font-bold">Loading, please wait...</p>
         </div>
     );
 };
-
 export default Spinner;
